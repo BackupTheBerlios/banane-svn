a4bdb3d25e4685b6aac533dd160121071dd32e90
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-01-18 15:50:17 +0000

    Test again

--- trunk/Support/kwextract.m
+++ trunk/Support/kwextract.m
@@ -6,7 +6,7 @@
 %  $Id$
 %
 % AIM:
-%  Bla18, Extracts keywords and their values from function calls.
+%  Bla19, Extracts keywords and their values from function calls.
 %
 % DESCRIPTION:
 %  kwextract enables argument passing to a function in the form
