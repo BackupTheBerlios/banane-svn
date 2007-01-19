8954ea03d2347b900a0a99f59d8e24f9693909b7
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-01-19 13:42:03 +0000

    Putting PHP scripts under version control.

---
+++ trunk/Documentation/PHP/viewdir.php
@@ -0,0 +1,45 @@
+<HTML> 
+<?php
+
+## needed to call configuration script that knows everything else:
+$bananepath="/home/groups/banane/htdocs/wwwcopy/Banane/";
+
+## get info from config script.
+$confscr=$bananepath."Documentation/Scripts/wwwdocu_conf.scr";
+
+$dbscr=$confscr." db";
+$allout=`$dbscr`;
+$out=explode("\n",$allout);
+$dbserver=$out[0];
+$dbname=$out[1];
+$dbuser=$out[2];
+$dbpasswd=$out[3];
+$db = mysql_connect($dbserver, $dbuser, $dbpasswd); 
+mysql_select_db($dbname,$db);
+
+$webscr=$confscr." web";
+$allout=`$webscr`;
+$out=explode("\n",$allout);
+$webpath=$out[0];
+
+## use the first argument to the script as the directory to display.
+$dirname=($argv[0]);
+
+## select all routines in respective directory and in its subdirs.
+## % in database query is the wildcard character.
+$querystring= "SELECT name,aim FROM routines WHERE fullpath LIKE '".$dirname."%'";
+$routines = mysql_query($querystring,$db); 
+
+## display table with name and aim column. make the name a link to the
+## viewroutine script with the routine name as the argument
+echo "<B>Contents of directory <I>".$dirname."</I></B>";
+echo "<TABLE>";
+while($rrow = mysql_fetch_array($routines))
+  { $rname=$rrow["name"];
+    $anchor=$webpath."Documentation/PHP/viewroutine.php?".$rname;
+    echo "<TD VALIGN=TOP><A HREF='".$anchor."'>".$rname."</A>";
+    echo "<TD VALIGN=TOP>".$rrow["aim"]."</TR>";
+  }
+echo "</TABLE>";
+?> 
+</HTML>
---
+++ trunk/Documentation/PHP/viewroutine.php
@@ -0,0 +1,69 @@
+<HTML> 
+<?php
+$bananepath="/home/groups/banane/htdocs/wwwcopy/Banane/";
+
+$conscr=$bananepath."Documentation/Scripts/wwwdocu_conf.scr db";
+$allout=`$conscr`;
+$out=explode("\n",$allout);
+$dbserver=$out[0];
+$dbname=$out[1];
+$dbuser=$out[2];
+$dbpasswd=$out[3];
+$db = mysql_connect($dbserver, $dbuser, $dbpasswd); 
+mysql_select_db($dbname,$db);
+
+$rname=($argv[0]);
+
+$querystring= "SELECT * FROM routines WHERE name='".$rname."'";
+$routine = mysql_query($querystring,$db); 
+$querystring= "SELECT * FROM inputs WHERE name='".$rname."'";
+$inputs = mysql_query($querystring,$db); 
+$querystring= "SELECT * FROM optinputs WHERE name='".$rname."'";
+$optinputs = mysql_query($querystring,$db); 
+$querystring= "SELECT * FROM outputs WHERE name='".$rname."'";
+$outputs = mysql_query($querystring,$db); 
+$rrow = mysql_fetch_array($routine);  
+echo "<TABLE>"; 
+echo "<TR><TD VALIGN=TOP><B>Name</B>";
+echo "<TD VALIGN=TOP>".$rrow["name"]."</TR>";
+echo "<TR><TD VALIGN=TOP><B>Full path</B>";
+echo "<TD VALIGN=TOP>".$rrow["fullpath"]."</TR>";
+echo "<TR><TD VALIGN=TOP><B>Aim</B>";
+echo "<TD VALIGN=TOP>".$rrow["aim"]."</TR>";
+echo "<TR><TD VALIGN=TOP><B>Description</B>"; 
+echo "<TD VALIGN=TOP>".$rrow["description"]."</TR>";
+echo "<TR><TD VALIGN=TOP><B>Category</B>"; 
+echo "<TD VALIGN=TOP>".$rrow["category"]."</TR>";
+echo "<TR><TD VALIGN=TOP><B>Inputs</B><TD VALIGN=TOP>"; 
+echo "<TABLE>";
+while($irow = mysql_fetch_array($inputs)) 
+{ 
+  echo "<TR><TD VALIGN=TOP><I>".$irow["argument"].": </I>";
+  echo "<TD VALIGN=TOP>".$irow["description"];
+  echo "</TR>";
+ }  
+echo "</TABLE>";
+echo "</TR>"; 
+echo "<TR><TD VALIGN=TOP><B>Optional inputs</B><TD VALIGN=TOP>"; 
+echo "<TABLE>";
+while($oirow = mysql_fetch_array($optinputs)) 
+{ 
+  echo "<TR><TD VALIGN=TOP><I>".$oirow["argument"].": </I>";
+  echo "<TD VALIGN=TOP>".$oirow["description"];
+  echo "</TR>";
+ }  
+echo "</TABLE>";
+echo "</TR>"; 
+echo "<TR><TD VALIGN=TOP><B>Outputs</B><TD VALIGN=TOP>"; 
+echo "<TABLE>";
+while($orow = mysql_fetch_array($outputs)) 
+{ 
+  echo "<TR><TD VALIGN=TOP><I>".$orow["argument"].": </I>";
+  echo "<TD VALIGN=TOP>".$orow["description"];
+  echo "</TR>";
+ }  
+echo "</TABLE>";
+echo "</TR>"; 
+echo "</TABLE>"; 
+?> 
+</HTML>
