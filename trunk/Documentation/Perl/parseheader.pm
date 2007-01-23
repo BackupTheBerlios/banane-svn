5629f89f661e76e267aa08c658b39454811ce978
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-01-23 16:44:27 +0000

    Testing code environment

--- trunk/Documentation/Perl/parseheader.pm
+++ trunk/Documentation/Perl/parseheader.pm
@@ -110,7 +110,7 @@ $grammar =
     example : "%" /\ */ "EXAMPLE:" nl headerline(s)
      { $jo=join(" ",@{$item[-1]});
 print "$jo\n";
-$jo =~ s/\<\/CODE\>\<BR\>\<BR\>\<CODE\>/\<BR\>/g;
+$jo =~ s/CODE/code123/g;
 print "$jo\n";
 $return=$jo }
 
