1ed5eadd9cd54620580a8dbf2d0e826dfea2aab6
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-01-23 13:16:25 +0000

    String concatenation in optional sections.

--- trunk/Documentation/Perl/parseheader.pm
+++ trunk/Documentation/Perl/parseheader.pm
@@ -191,7 +191,8 @@ $result=$parse->code($_[0]);
 my $restr="";
 foreach (@{$result->[11]->[0]}) {$restr=$restr." ".$_->[1];}
 
-print $$result[11]."\n";
+print ref($$result[11])."\n";
+print ${$$result[11]}."\n";
 
 my $al="";
 foreach (@{$result->[14]->[0]}) {$al=$al.$_->[1];}
