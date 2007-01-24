8aee1fad0e4c327e3a7aa1907ef6340fedfe81fa
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-01-24 10:11:13 +0000

    Expanding references.

--- trunk/Documentation/PHP/viewroutine.php
+++ trunk/Documentation/PHP/viewroutine.php
@@ -90,7 +90,8 @@ if($rrow["also"]!="NULL")
   $also=$rrow["also"];
 #  echo $also."\n";
 #  $pattern="<A>[A-Za-z0-9_]<\/A>";
-  $pattern="<A>+eNa+</A>";
+#  $pattern="<A>+eNa+</A>";
+  $pattern="*eNa*";
   preg_match_all($pattern, $also, $refnames, PREG_SET_ORDER);
 #  print_r($refnames);
 echo $refnames[0][0] . ", " . $refnames[0][1] . "\n";
