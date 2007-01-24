18d1cf6cb738ecc4b030207c64fca5cdfdd60d2a
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-01-24 09:54:04 +0000

    Expanding references.

--- trunk/Documentation/PHP/viewroutine.php
+++ trunk/Documentation/PHP/viewroutine.php
@@ -88,8 +88,8 @@ echo "<TD VALIGN=TOP>".$rrow["example"]."</TR>";
 if($rrow["also"]!="NULL") 
 { 
   $also=$rrow["also"];
-echo $also"
-  $pattern="<A>[A-Za-z0-9_]</A>";
+  echo $also. "\n";
+  $pattern="<A>[A-Za-z0-9_]<\/A>";
   preg_match_all($pattern, $also, $refnames);
 #  print_r($refnames);
 echo $refnames[0][0] . ", " . $refnames[0][1] . "\n";
