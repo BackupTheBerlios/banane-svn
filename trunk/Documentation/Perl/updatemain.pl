6393fb4faac16eb2faa144338549150753ab329b
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-01-25 16:25:12 +0000

    multi delete before input insertion

--- trunk/Documentation/Perl/updatemain.pl
+++ trunk/Documentation/Perl/updatemain.pl
@@ -47,19 +47,19 @@ if ($file->[0]) {
   die "Couldn't prepare query; aborting"
     unless defined $routines_replacehandle;
 
-  my $all_deletehandle = $dbh->prepare_cached("DELETE inputs,optinputs,outputs FROM routines,inputs,optinputs,outputs WHERE routines.name = ?");
+  my $all_deletehandle = $dbh->prepare("DELETE inputs,optinputs,outputs FROM routines,inputs,optinputs,outputs WHERE routines.name = ?");
   die "Couldn't prepare query; aborting"
     unless defined $all_deletehandle;
 
-  my $inputs_replacehandle = $dbh->prepare_cached("INSERT INTO inputs (name,argument,description) VALUES (?,?,?)");
+  my $inputs_replacehandle = $dbh->prepare("INSERT INTO inputs (name,argument,description) VALUES (?,?,?)");
   die "Couldn't prepare query; aborting"
     unless defined $inputs_replacehandle;
 
-  my $optinputs_replacehandle = $dbh->prepare_cached("REPLACE INTO optinputs (name,argument,description) VALUES (?,?,?)");
+  my $optinputs_replacehandle = $dbh->prepare("INSERT INTO optinputs (name,argument,description) VALUES (?,?,?)");
   die "Couldn't prepare query; aborting"
     unless defined $optinputs_replacehandle;
 
-  my $outputs_replacehandle = $dbh->prepare_cached("REPLACE INTO outputs (name,argument,description) VALUES (?,?,?)");
+  my $outputs_replacehandle = $dbh->prepare("INSERT INTO outputs (name,argument,description) VALUES (?,?,?)");
   die "Couldn't prepare query; aborting"
     unless defined $outputs_replacehandle;
 
