ae7e92a4e7fa91c07edac0e51998461495d99266
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-01-31 13:33:12 +0000

    added new scripts for homepage display

--- trunk/Documentation/PHP/index.php
+++ trunk/Documentation/PHP/index.php
@@ -1,9 +1,8 @@
+<?php
 ## index.php calls the mainpage script that actually 
 ## displays the banane homepage. copy index.php to the 
 ## initial directory where the browser looks for it.
 ## be shure to correctly modify the $bananepath.
-
-<?php
 
 $bananepath="/home/groups/banane/htdocs/wwwcopy/Banane/";
 
