a6d46401f0f64eac689c888ce828186b7bad2cb4
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-02-16 12:29:10 +0000

    Added new directory for debugging purpose.

---
+++ trunk/foodir/testtemplate.m
@@ -0,0 +1,79 @@
+%+
+% NAME:
+%  example_function()
+%
+% VERSION:
+%  $Id:$
+%
+% AUTHOR:
+%  J. R. Hacker
+%
+% DATE CREATED:
+%  9/2002
+%
+% AIM:
+%  Short description of the routine in a single line.
+%
+% DESCRIPTION:
+%  Detailed description of the routine. The text may contain small HTML
+%  tags like for example <BR> linebreaks or <VAR>variable name
+%  typesetting</VAR>. Simple anchors to other banane routines are
+%  also allowed, eg <A>kwextract</A>.
+%
+% CATEGORY:
+%  At present, there are the following possibilities:<BR>
+%   - DataStructures<BR>
+%   - Documentation<BR>
+%   - NEV Tools<BR>
+%   - Support routines<BR>
+%  Others may be invented, with corresponding subdirectories in the
+%  BANANE directory tree. For example:<BR>
+%   - Array<BR>
+%   - DataStorage<BR>
+%   - Demonstration<BR>
+%   - Graphic<BR>
+%   - Help<BR>
+%   - Statistics<BR>
+%   - Signals<BR>
+%   - Simulation<BR>
+%   - Strings<BR>
+%
+% SYNTAX:
+%* result = example_function(arg1, arg2,[, optarg1][, optarg2]); 
+%
+% INPUTS:
+%  arg1:: First argument of the function call. Indicate variable type and
+%  function.
+%  arg2:: Second argument of the function call.
+%
+% OPTIONAL INPUTS:
+%  optarg1:: An optional input argument.
+%  optarg2:: Another optional input argument.
+%
+% OUTPUTS:
+%  result:: The result of the routine.
+%
+% RESTRICTIONS:
+%  Optional section: Is there anything known that could cause problems?
+%
+% PROCEDURE:
+%  Short description of the algorithm.
+%
+% EXAMPLE:
+%  Indicate example lines with * as the first character. These lines
+%  will be typeset in a fixed width font. 
+%* data=example_function(23,5)
+%  
+%  Indicate matlab output with *>
+%*> ans =
+%*>   28
+%
+% SEE ALSO:
+%  Optional section: Mention related or required files here. Banane routines may be refenced as anchors <A>loadNEV</A>. 
+%-
+
+
+
+function result = funcname(arg1,arg2);
+  
+  result = arg1+arg2;
