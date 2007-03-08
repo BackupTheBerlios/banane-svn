5b6f43b0e4d6e57224a333ac878cb22c95ce1ffa
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-03-08 13:38:39 +0000

    new homepage design

--- trunk/Documentation/PHP/viewtree.php
+++ trunk/Documentation/PHP/viewtree.php
@@ -67,7 +67,7 @@ function directory_to_list($dir,$onlydirs=FALSE,$sub=FALSE)
 
 // if you want to list directories only, use
 echo "<body background='Pics/l02.gif' leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>";
-echo "<SPAN class='navbar'><TREE>";</SPAN>"
+echo "<SPAN class='navbar'><TREE>";
 echo "<h1>directories</h1>";
 echo directory_to_list('/home/groups/banane/htdocs/wwwcopy/Banane',TRUE);
 echo "</SPAN>";
