372af76a7fe60606954189924400e717bbc578e0
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-03-08 15:08:00 +0000

    new homepage design

--- trunk/Documentation/PHP/viewtree.php
+++ trunk/Documentation/PHP/viewtree.php
@@ -72,7 +72,7 @@ echo "<h1>directories</h1>";
 echo directory_to_list('/home/groups/banane/htdocs/wwwcopy/Banane',TRUE);
 echo "<h1>search</h1>";
 echo "<form name='input' target='dynamic' action='viewsearch.php' method='get'>";
-echo "<input type='text' name='routine'>";
+echo "<input size=7 type='text' name='routine'>";
 echo "<input type='submit' value='Go!'>";
 echo "</form>";
 echo "</DIV>";
