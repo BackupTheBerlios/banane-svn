3d7257a551dc532464eb0975bad11bbd1de86333
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-03-08 14:00:27 +0000

    new homepage design

--- trunk/Documentation/PHP/viewtree.php
+++ trunk/Documentation/PHP/viewtree.php
@@ -67,11 +67,10 @@ function directory_to_list($dir,$onlydirs=FALSE,$sub=FALSE)
 
 // if you want to list directories only, use
 echo "<body background='Pics/l02.gif' leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>";
-//echo "<SPAN class='navbar'><tree>blablabla</SPAN>";
+echo "<SPAN class='navbar'><tree>blablabla";
 echo "<h1>directories with pos?</h1>";
 echo directory_to_list('/home/groups/banane/htdocs/wwwcopy/Banane',TRUE);
-//echo "</SPAN>";
-echo "<SPAN class='navbar'><TREE>blablabla</SPAN>";
+echo "</SPAN>";
 echo "<h1>search</h1>";
 echo "<form name='input' target='dynamic' action='viewsearch.php' method='get'>";
 echo "<input type='text' name='routine'>";
