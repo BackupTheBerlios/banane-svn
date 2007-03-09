2c219bd85a22e6b8e466b303211401908e390d0c
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-03-09 12:20:33 +0000

    new homepage design

--- trunk/Documentation/PHP/links.php
+++ trunk/Documentation/PHP/links.php
@@ -67,10 +67,10 @@ function directory_to_list($dir,$onlydirs=FALSE,$sub=FALSE)
 
 // if you want to list directories only, use
 //echo "<body background='Pics/l02.gif' leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>";
-echo "<body>";
-echo "<img src='Pics/l02.gif' margin=0 border=0 usemap='#mymap'>";
+echo "<body leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>";
+echo "<img src='Pics/l02.gif' usemap='#mymap'>";
 echo "<map name='mymap'>";
-echo " <area href='mainpage.php' alt='banane' shape=rect coords='0,0,118,28'>";
+//echo " <area href='mainpage.php' alt='banane' shape=rect coords='0,0,118,28'>";
 echo " <area target='_blank' href='http://developer.berlios.de'
 	title='berliOS developer' alt='berliOS' shape=rect coords='0,0,10,10'>";
 echo "</map>";
