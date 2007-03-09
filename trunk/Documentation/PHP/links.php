0ae3e3f39bc6e01e339926172b84a021b7d2a7c0
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-03-09 16:32:03 +0000

    input form style

--- trunk/Documentation/PHP/links.php
+++ trunk/Documentation/PHP/links.php
@@ -84,7 +84,7 @@ echo directory_to_list('/home/groups/banane/htdocs/wwwcopy/Banane',TRUE);
 echo "<h1>search</h1>";
 echo "<form name='input' target='dynamic' action='viewsearch.php' method='get'>";
 echo "<input size=7 type='text' name='routine'>";
-echo "<input type='submit' value='Go!'>";
+echo "<p class='go'><input type='submit' value='Go!'></p>";
 echo "</form>";
 echo "</DIV>";
 echo "</body>";
