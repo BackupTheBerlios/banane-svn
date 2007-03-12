8cc5b0fca3c6738e00275158548d664d23d169d9
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-03-12 09:00:34 +0000

    layout of web docu

--- trunk/Documentation/PHP/links.php
+++ trunk/Documentation/PHP/links.php
@@ -83,7 +83,7 @@ echo "<h1>directories</h1>";
 echo directory_to_list('/home/groups/banane/htdocs/wwwcopy/Banane',TRUE);
 echo "<h1>search</h1>";
 echo "<form name='input' target='dynamic' action='viewsearch.php' method='get'>";
-echo "<input type='text' name='routine'>";
+echo "<input style='width:100%;' type='text' name='routine'>";
 //echo "<p style='text-align:right; margin-top:2px'><input type='submit' value='go!' class='btn'></p>";
 echo "</form>";
 echo "</DIV>";
