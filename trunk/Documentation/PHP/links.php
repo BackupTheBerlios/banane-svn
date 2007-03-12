f937d221bfff243c17d21e1f549654c8afa17f96
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-03-12 10:46:53 +0000

    introducing absolute links

--- trunk/Documentation/PHP/links.php
+++ trunk/Documentation/PHP/links.php
@@ -65,24 +65,36 @@ function directory_to_list($dir,$onlydirs=FALSE,$sub=FALSE)
 
 // *** main html code generation starts here
 
-//echo "<body background='Pics/l02.gif' leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>";
+$bananepath="/home/groups/banane/htdocs/wwwcopy/Banane/";
+
+## get info from config script.
+$confscr=$bananepath."Documentation/Scripts/wwwdocu_conf.scr";
+
+$webscr=$confscr." web";
+$allout=`$webscr`;
+$out=explode("\n",$allout);
+$webpath=$out[0];
+
+$phppath=$webpath."Documentation/PHP/";
+$picpath=$phppath."Pics/";
+
 echo "<body leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>";
-echo "<img src='Pics/l02.gif' border=0 margin=0 padding=0 usemap='#mymap'>";
+echo "<img src=".$picpath."l02.gif' border=0 margin=0 padding=0 usemap='#mymap'>";
 echo "<map name='mymap'>";
-echo " <area target='_top' href='mainpage.php' title='banane mainpage' alt='banane' shape=rect coords='12,16,136,150'>";
+echo " <area target='_top' href='".$phppath."mainpage.php' title='banane mainpage' alt='banane' shape=rect coords='12,16,136,150'>";
 echo " <area target='_blank' href='http://developer.berlios.de' title='berliOS developer' alt='berliOS' shape=rect coords='24,572,90,620'>";
 echo "</map>";
 echo "<DIV class='navbar'>";
 echo "<h1>project</h1>";
 echo "<ul>";
-echo "<li><a target='dynamic' href='intro.html'>about</a></li>";
+echo "<li><a target='dynamic' href='".$phppath."intro.html'>about</a></li>";
 echo "<li><a target='_blank' href='http://project-banane.blogspot.com'>weblog</a></li>";
 echo "<li><a>members</a></li>";
 echo "</ul>";
 echo "<h1>directories</h1>";
 echo directory_to_list('/home/groups/banane/htdocs/wwwcopy/Banane',TRUE);
 echo "<h1>search</h1>";
-echo "<form name='input' target='dynamic' action='viewsearch.php' method='get'>";
+echo "<form name='input' target='dynamic' action='".$phppath."viewsearch.php' method='get'>";
 echo "<p style='margin-top:2px'><input style='width:100%;' type='text' name='routine'></p>";
 //echo "<p style='text-align:right; margin-top:2px'><input type='submit' value='go!' class='btn'></p>";
 echo "</form>";
