bd184e310d66006da7dc1c5cede69fe72ce3c223
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-06-06 09:05:14 +0000

    anchor bug search

--- trunk/Documentation/PHP/viewroutine.php
+++ trunk/Documentation/PHP/viewroutine.php
@@ -19,25 +19,19 @@ display its header information in a formatted manner. -->
 ## support function to search simplified anchors <A>routine</A> 
 ## and replace them with with proper hyperrefs
 function anchorreplace($original,$path){
-#  $pattern="/(<(A|a)[^>]*>)(.*)(<\/\\2>)/";
-#  $pattern="/(<(A|a)[^>])/";
-  $pattern="/(<([\w]+)[^>]*>)(.*)(<\/\\2>)/U";
-  preg_match_all($pattern, $original, $matches, PREG_SET_ORDER);
-#  echo "matches: ";
-  print_r($matches);
-  foreach ($matches as $val) {
-    echo "matched: " . $val[0] . "\n";
-    echo "part 1: " . $val[1] . "\n";
-    echo "part 2: " . $val[3] . "\n";
-    echo "part 3: " . $val[4] . "\n\n";
-    $search="/(<(A|a)[^>]*>)".$val[3]."/";
-#    echo "search: ".$search."\n";
-    $replace="<A href='".$path."Documentation/PHP/viewroutine.php?routine=".$val[3]."'>".$val[3];
-    $original = preg_replace($search, $replace, $original); 
-##    $echo "original: ".$original."\n";
-  }
-  return $original;
-}
+   $pattern="/(<(A|a)[^>]*>)(.*)(<\/\\2>)/U";
+   preg_match_all($pattern, $original, $matches, PREG_SET_ORDER);
+   foreach ($matches as $val) {
+#    echo "matched: " . $val[0] . "\n";
+#    echo "part 1: " . $val[1] . "\n";
+#    echo "part 2: " . $val[3] . "\n";
+#    echo "part 3: " . $val[4] . "\n\n";
+     $search="/(<(A|a)[^>]*>)".$val[3]."/";
+     $replace="<A href='".$path."Documentation/PHP/viewroutine.php?routine=".$val[3]."'>".$val[3];
+     $original = preg_replace($search, $replace, $original); 
+   }
+   return $original;
+ }
 
 
 $bananepath="/home/groups/banane/htdocs/wwwcopy/Banane/";
