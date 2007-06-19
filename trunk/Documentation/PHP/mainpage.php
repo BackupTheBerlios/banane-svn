2f693f8f0d5556ff4ec01c373a30b660e062ae11
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-06-19 15:33:11 +0000

    removed doctype

--- trunk/Documentation/PHP/mainpage.php
+++ trunk/Documentation/PHP/mainpage.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD html 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html;charset=ISO-8859-1">
