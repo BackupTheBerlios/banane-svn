cf126c635dd33d2717132d1f9f4506abb682eb1c
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2007-07-13 10:45:14 +0000

    Improved docu and added 'mirror' and 'lsg' options.

--- trunk/DataStructures/triggers.m
+++ trunk/DataStructures/triggers.m
@@ -19,7 +19,7 @@
 %  triggers() function is meant to provide a common basis that extracts
 %  the trigger signals from a NEV structure according to the special
 %  needs of the respective experiment. triggers() needs an input string that
-%  defines how to determine the trigger signals for the respcetive
+%  defines how to determine the trigger signals for the respective
 %  experiment. The routine needs to be extended if new experiments
 %  require different trigger evaluations. 
 %
@@ -35,13 +35,16 @@
 %  experiment:: String variable defining the type of trigger extraction
 %               that has to be carried out. Presently, the following
 %               options exist: <BR>
-%               '070704-02'|'070704-05' Compute triggers by combining
+%               '070704-02'|'070704-05'|'lsg'<BR> 
+%               Compute triggers by combining
 %               the timestamps recorded on the analog channels 1 and
 %               2. This method is suitable for triggers provided by the
 %               standalone light stimulus generator. <BR>
-%               '070704-04','051123-02','051123-10','051123-14' Compute
+%               '070704-04'|'051123-02'|'051123-10'|'051123-13'
+%               |'051123-14'|'mirror'<BR>
+%               Compute
 %               triggers by choosing either the 
-%               timestamps recorded on the analog channels 3 or 4. The
+%               timestamps recorded on the analog channel 3 or 4. The
 %               channel with the larger first trigger interval is
 %               chosen. This method is suitable for triggers provided by the
 %               mirror control system.
@@ -65,12 +68,12 @@ function trigstamps=triggers(nev,experiment)
   
   switch experiment
    
-   case {'070704-02','070704-05'}
+   case {'lsg','070704-02','070704-05'}
     raw=[nev.ExpData.analog(1).timestamps ...
          nev.ExpData.analog(2).timestamps];
     trigstamps=sort(raw);
    
-   case {'070704-04','051123-02','051123-10','051123-13','051123-14'}
+   case {'mirror','070704-04','051123-02','051123-10','051123-13','051123-14'}
     % Extract the "right"
     % trigger channel, which is characterized by an interval
     % between first and second trigger of nearly 500ms, instead of about
