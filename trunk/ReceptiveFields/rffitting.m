7d198ab360559eabebda401f3bd822e0982ef471
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2008-01-14 13:43:11 +0000

    No changes?

--- trunk/ReceptiveFields/rffitting.m
+++ trunk/ReceptiveFields/rffitting.m
@@ -68,7 +68,7 @@ function rfstruct=rffitting(rfstruct,varargin)
   disp('RFFitting');
   disp('Processing prototypes.');
 
-  for pidx=1:rfstruct.nproto
+  for pidx=1:2%rfstruct.nproto
 
     if (not(kw.graphic))
       %% simple output to show progress
@@ -79,129 +79,129 @@ function rfstruct=rffitting(rfstruct,varargin)
     end
     
     
-  mav=squeeze(max(rfstruct.pr(pidx).spacetime,[],1));
-  sumav=squeeze(sum(rfstruct.pr(pidx).spacetime,1));
+    mav=squeeze(max(rfstruct.pr(pidx).spacetime,[],1));
+    sumav=squeeze(sum(rfstruct.pr(pidx).spacetime,1));
   
 
-  [xmax,ix]=max(max(mav,[],1));
-  [ymax,iy]=max(max(mav,[],2));
+    [xmax,ix]=max(max(mav,[],1))
+    [ymax,iy]=max(max(mav,[],2))
     
-  tc=(rfstruct.pr(pidx).spacetime(:,iy,ix));
+    tc=(rfstruct.pr(pidx).spacetime(:,iy,ix));
   
-  meantc=mean(tc);
-  stdtc=std(tc);
+    meantc=mean(tc);
+    stdtc=std(tc);
 
-  [mtc,itc]=max(tc);
+    [mtc,itc]=max(tc);
   
   
-  % find the start and stop of the first peak
-  tczero=find((tc-(meantc+stdtc))<=0);
+    % find the start and stop of the first peak
+    tczero=find((tc-(meantc+stdtc))<=0);
   
-  startidx=find((tczero-itc) < 0,1,'last');
-  starttime=tczero(startidx)+1;
-  if (startidx==length(tczero))
-    stoptime=rfstruct.avtime;
-  else
-    stoptime=tczero(startidx+1)-1;
-  end
-  
-  rf2=squeeze(sum(rfstruct.pr(pidx).spacetime(starttime:stoptime,:,:),1));
-  rf=max(rf2-mean(rf2(:)),zeros(rfstruct.xyextent));
-  
-  
-  if (nnz(rf)~=0)
-    vstart=[iy ix max(rf(:)) 1.5 1.5];
-    [v,fval] = fminsearch(@(v) fitgauss2d(v,rf),vstart,optimset('Display','off'));
-  else
-    v = [ones(1,5)];
-    fval=Inf;
-  end
-  
-  gs=v(3)*gauss2d(rfstruct.xyextent(2),rfstruct.xyextent(1),v(1),v(2),v(4),v(5));
-  mask2sig=(gs>=v(3)*exp(-0.5*kw.avspacerange.^2));
-  
-  vmm=v;
-  vmm(1)=rfstruct.xmmperpos*(v(1)-rfstruct.xyextent(2)/2);%mmynz(round(v(1)));
-  vmm(2)=rfstruct.ymmperpos*(v(2)-rfstruct.xyextent(1)/2);%mmxnz(round(v(2)));
-  vmm(4)=rfstruct.xmmperpos*v(4);
-  vmm(5)=rfstruct.ymmperpos*v(5);
-
-  [ymaskidx,xmaskidx]=find(mask2sig);
-  
-  tmpav=zeros(rfstruct.avtime,length(xmaskidx));
-  
-  for avidx=1:length(xmaskidx)
-    tmpav(:,avidx)=rfstruct.pr(pidx).spacetime(:,ymaskidx(avidx),xmaskidx(avidx));
-  end
-  
-  tcav=mean(tmpav,2);
-  
-  rfstruct.pr(pidx).fit=[vmm fval];
-  rfstruct.pr(pidx).avspace=rf2;
-  rfstruct.pr(pidx).avtime=tcav;
-
-  if (kw.graphic)
-  
-    disp(['index: ' num2str(pidx)]);
-    disp(['electrode: ' num2str(rfstruct.pr(pidx).eln)]);
-    disp(['prototype: ' num2str(rfstruct.pr(pidx).prn)]);
-    disp(' ');
+    startidx=find((tczero-itc) < 0,1,'last');
+    starttime=tczero(startidx)+1;
+    if (startidx==length(tczero))
+      stoptime=rfstruct.avtime;
+    else
+      stoptime=tczero(startidx+1)-1;
+    end
     
-    subplot(2,4,1), imagesc(rfstruct.mmxnz,rfstruct.mmynz,sumav);
-    axis xy;
-    xlabel('x / mm');
-    ylabel(' y / mm');
-    title('Sum of complete time interval');
-    
-    subplot(2,4,2), plot(tc);
-    axis tight;
-    xlabel('t / ms');
-    ylabel('???');
-    title('Time course at maximum position');
-    hold on
-    subplot(2,4,2), plot(xlim,[meantc meantc]);
-    subplot(2,4,2), plot(xlim,[meantc+stdtc meantc+stdtc]);
-    hold off
-
-    subplot(2,4,3), imagesc(rfstruct.mmxnz,rfstruct.mmynz,rf)
-    axis xy;
-    xlabel('x / mm');
-    ylabel(' y / mm');
-    title('Sum of peak time interval');
-    hold on
-    emm=ellipse(25,vmm(2),vmm(1),2*vmm(5),2*vmm(4));
-    plot(emm(1,:),emm(2,:),'w','LineWidth',2);
-    plot(vmm(2),vmm(1),'wo','LineWidth',2);
-    hold off
-
-    subplot(2,4,4),plot(rfstruct.mmxnz,rf(round(v(1)),:))
-    axis tight;
-    xlabel('x / mm');
-    ylabel('???');
-    title('X Slice at maximum position');
-    hold on
-    subplot(2,4,4),plot(rfstruct.mmxnz,gs(round(v(1)),:),'r')
-    hold off
-
-    subplot(2,4,5),plot(rfstruct.mmynz,rf(:,round(v(2)))) % , imagesc(gs);
-    axis tight;
-    xlabel('y / mm');
-    ylabel('???');
-    title('Y Slice at maximum position');
-    hold on
-    subplot(2,4,5),plot(rfstruct.mmynz,gs(:,round(v(2))),'r')
-    hold off
+    rf2=squeeze(sum(rfstruct.pr(pidx).spacetime(starttime:stoptime,:,:),1));
+    rf=max(rf2-mean(rf2(:)),zeros(rfstruct.xyextent));
   
-    subplot(2,4,6), imagesc(rfstruct.mmxnz,rfstruct.mmynz,mask2sig.*rf);
-    axis xy;
-    xlabel('x / mm');
-    ylabel(' y / mm');
+  
+    if (nnz(rf)~=0)
+      vstart=[iy ix max(rf(:)) 1.5 1.5];
+      [v,fval] = fminsearch(@(v) fitgauss2d(v,rf),vstart,optimset('Display','off'));
+    else
+      v = [ones(1,5)];
+      fval=Inf;
+    end
+  
+    gs=v(3)*gauss2d(rfstruct.xyextent(2),rfstruct.xyextent(1),v(1),v(2),v(4),v(5));
+    mask2sig=(gs>=v(3)*exp(-0.5*kw.avspacerange.^2));
+  
+    vmm=v;
+    vmm(1)=rfstruct.ymmperpos*(v(1)-rfstruct.xyextent(2)/2);%mmynz(round(v(1)));
+    vmm(2)=rfstruct.xmmperpos*(v(2)-rfstruct.xyextent(1)/2);%mmxnz(round(v(2)));
+    vmm(4)=rfstruct.ymmperpos*v(4);
+    vmm(5)=rfstruct.xmmperpos*v(5);
 
-    subplot(2,4,7), plot(tcav);
-    axis tight;
-    xlabel('t / ms');
-    pause
-  end
+    [ymaskidx,xmaskidx]=find(mask2sig);
+  
+    tmpav=zeros(rfstruct.avtime,length(xmaskidx));
+  
+    for avidx=1:length(xmaskidx)
+      tmpav(:,avidx)=rfstruct.pr(pidx).spacetime(:,ymaskidx(avidx),xmaskidx(avidx));
+    end
+  
+    tcav=mean(tmpav,2);
+  
+    rfstruct.pr(pidx).fit=[vmm fval];
+    rfstruct.pr(pidx).avspace=rf2;
+    rfstruct.pr(pidx).avtime=tcav;
+    
+    if (kw.graphic)
+  
+      disp(['index: ' num2str(pidx)]);
+      disp(['electrode: ' num2str(rfstruct.pr(pidx).eln)]);
+      disp(['prototype: ' num2str(rfstruct.pr(pidx).prn)]);
+      disp(' ');
+    
+      subplot(2,4,1), imagesc(rfstruct.mmxnz,rfstruct.mmynz,sumav);
+      axis xy;
+      xlabel('x / mm');
+      ylabel(' y / mm');
+      title('Sum of complete time interval');
+    
+      subplot(2,4,2), plot(tc);
+      axis tight;
+      xlabel('t / ms');
+      ylabel('???');
+      title('Time course at maximum position');
+      hold on
+      subplot(2,4,2), plot(xlim,[meantc meantc]);
+      subplot(2,4,2), plot(xlim,[meantc+stdtc meantc+stdtc]);
+      hold off
+      
+      subplot(2,4,3), imagesc(rfstruct.mmxnz,rfstruct.mmynz,rf)
+      axis xy;
+      xlabel('x / mm');
+      ylabel(' y / mm');
+      title('Sum of peak time interval');
+      hold on
+      emm=ellipse(25,vmm(2),vmm(1),2*vmm(5),2*vmm(4));
+      plot(emm(1,:),emm(2,:),'w','LineWidth',2);
+      plot(vmm(2),vmm(1),'wo','LineWidth',2);
+      hold off
+
+      subplot(2,4,4),plot(rfstruct.mmxnz,rf(round(v(1)),:))
+      axis tight;
+      xlabel('x / mm');
+      ylabel('???');
+      title('X Slice at maximum position');
+      hold on
+      subplot(2,4,4),plot(rfstruct.mmxnz,gs(round(v(1)),:),'r')
+      hold off
+
+      subplot(2,4,5),plot(rfstruct.mmynz,rf(:,round(v(2)))) % , imagesc(gs);
+      axis tight;
+      xlabel('y / mm');
+      ylabel('???');
+      title('Y Slice at maximum position');
+      hold on
+      subplot(2,4,5),plot(rfstruct.mmynz,gs(:,round(v(2))),'r')
+      hold off
+  
+      subplot(2,4,6), imagesc(rfstruct.mmxnz,rfstruct.mmynz,mask2sig.*rf);
+      axis xy;
+      xlabel('x / mm');
+      ylabel(' y / mm');
+      
+      subplot(2,4,7), plot(tcav);
+      axis tight;
+      xlabel('t / ms');
+      pause
+    end
   
  
   
