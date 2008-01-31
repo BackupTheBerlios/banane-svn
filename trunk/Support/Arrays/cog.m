98b07aef1cc379114c9650836e8d89e9e74e4cd4
ahjthiel <ahjthiel@a2d29e89-d622-0410-8339-be757d17a01b>
2008-01-31 10:39:38 +0000

    New routine.

---
+++ trunk/Support/Arrays/cog.m
@@ -0,0 +1,73 @@
+%+
+% NAME:
+%  cog()
+%
+% VERSION:
+%  $Id:$
+%
+% AUTHOR:
+%  M. T. Ahlers
+%
+% DATE CREATED:
+%  1/2008
+%
+% AIM:
+%  Compute center of gravity for two-dimensional array.
+%
+% DESCRIPTION:
+%  Detailed description of the routine. The text may contain small HTML
+%  tags like for example <BR> linebreaks or <VAR>variable name
+%  typesetting</VAR>. Simple anchors to other banane routines are
+%  also allowed, eg <A>kwextract</A>.
+%
+% CATEGORY:
+%  Support Routines<BR>
+%  Arrays
+%
+% SYNTAX:
+%* result = example_function(arg1, arg2 [,'optarg1',value][,'optarg2',value]); 
+%
+% INPUTS:
+%  arg1:: First argument of the function call. Indicate variable type and
+%  function.
+%  arg2:: Second argument of the function call.
+%
+% OPTIONAL INPUTS:
+%  optarg1:: An optional input argument.
+%  optarg2:: Another optional input argument. Of course, the whole
+%  section is optional, too.
+%
+% OUTPUTS:
+%  result:: The result of the routine.
+%
+% RESTRICTIONS:
+%  Optional section: Is there anything known that could cause problems?
+%
+% PROCEDURE:
+%  Short description of the algorithm.
+%
+% EXAMPLE:
+%  Indicate example lines with * as the first character. These lines
+%  will be typeset in a fixed width font. Indicate user input with >>. 
+%* >> data=example_function(23,5)
+%* ans =
+%*   28
+%
+% SEE ALSO:
+%  Optional section: Mention related or required files here. Banane routines may be refenced as anchors <A>loadNEV</A>. 
+%-
+
+function [row,col] = cog(im)
+  [m,n]=size(im);
+  r_sum=sum(im,2); 
+  c_sum=sum(im,1);
+  all_sum=sum(c_sum);
+  
+  if (all_sum~=0)
+    row=(r_sum'*[1:m]')/all_sum;
+    col=(c_sum*[1:n]')/all_sum;
+  else
+    warning('Sum of array is zero. Returning origin position as COG.')
+    row=1;
+    col=1;
+  end
